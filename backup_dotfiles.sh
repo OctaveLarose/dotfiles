1386d6621902750acc64ea68e5027087b9410775
Octave Larose <octave.larose@hotmail.fr>
2023-07-28 13:00:46 +0200

    libinput gestures basic config

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -16,7 +16,7 @@ cp -rv ~/bin .
 echo -e "${MSG_COLOR}\n### Backing up wallpapers...${NO_COLOR}"
 rm -rf ./wallpapers && mkdir ./wallpapers && cp -rf ~/Pictures/time_wallpapers/* ./wallpapers/ && echo "Done."
 
-CONFIG_LIST=("neofetch" "rofi" "dunst" "i3" "polybar" "galendae" "autorandr" "nushell")
+CONFIG_LIST=("neofetch" "rofi" "dunst" "i3" "polybar" "galendae" "autorandr" "nushell" "libinput-gestures.conf")
 
 echo -e "${MSG_COLOR}\n### Backing up config settings...${NO_COLOR}"
 for cfg in "${CONFIG_LIST[@]}"

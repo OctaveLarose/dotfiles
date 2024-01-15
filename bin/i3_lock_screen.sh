b1a77b589ba091b32e5e6dee4de9823a34104cd9
Octave Larose <octave.larose@hotmail.fr>
2024-01-15 10:08:21 +0000

    very minor tweaks to a few scripts

--- bin/i3_lock_screen.sh
+++ bin/i3_lock_screen.sh
@@ -1,5 +1,5 @@
 import -window root /tmp/screenshot.png
-convert /tmp/screenshot.png -blur 0x5 /tmp/screenshotblur.png
+convert /tmp/screenshot.png -blur 0x8 /tmp/screenshotblur.png
 rm /tmp/screenshot.png
 i3lock -i /tmp/screenshotblur.png
 rm /tmp/screenshotblur.png
--- bin/open_work_apps.sh
+++ bin/open_work_apps.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-pidof firefox || firefox &
-teams-for-linux &
-superproductivity &
-notion-app-enhanced &
-texstudio &
-obsidian &
-$HOME/Programs/PhD/git_repos/Zotero_linux-x86_64/zotero &
-spotify &
-youtube-music &
+commands=(
+    "firefox"
+    "teams-for-linux"
+    "superproductivity"
+    "notion-app-enhanced"
+    # "texstudio"
+    # "obsidian"
+    "$HOME/Programs/PhD/git_repos/Zotero_linux-x86_64/zotero"
+    "spotify"
+    "youtube-music"
+)
+
+for cmd in "${commands[@]}"; do
+    pidof "$cmd" || "$cmd" &
+done
+

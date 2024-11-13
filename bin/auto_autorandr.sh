d4b545ae18eef5997ef1ad49130adf045e1e117b
Octave Larose <octave.larose@hotmail.fr>
2024-11-13 13:31:21 +0000

    som-rs specific DAP debug + auto_autorandr back in action for now

--- bin/auto_autorandr.sh
+++ bin/auto_autorandr.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-STATUS_CMD="cat /sys/class/drm/card0/card0-HDMI-A-1/status"
+STATUS_CMD="cat /sys/class/drm/card1-HDMI-A-1/status /sys/class/drm/card1-DP-2/status"
 prev_status=$($STATUS_CMD)
 
 while true; do
   current_status=$($STATUS_CMD)
 
   if [[ "$current_status" != "$prev_status" ]]; then
-    autorandr --change
     prev_status=$current_status
+    autorandr --change
   fi
 
   sleep 1
--- config/nvim/after/ftplugin/rust.lua
+++ config/nvim/after/ftplugin/rust.lua
@@ -17,6 +17,93 @@ vim.keymap.set("n", "K", -- Override Neovim's built-in hover keymap with rustace
   { silent = true, buffer = bufnr }
 )
 
-vim.keymap.set("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>",
+vim.keymap.set("n", "<Leader>dt",
+  function()
+    vim.cmd('RustLsp testables')
+  end,
   { desc = "Debugger testables" }
 )
+
+local dap = require("dap")
+
+dap.adapters.rust = vim.g.rustaceanvim.dap.adapter -- is that really needed?
+
+-- should really be just in the som-rs project, but fine as is for now as it's my only active rust project
+dap.configurations.rust = {
+  {
+    name = "run-benchmark-ast",
+    type = "rust",
+    request = "launch",
+    program = function()
+      return vim.fn.getcwd() .. "/target/debug/som-interpreter-ast"
+    end,
+    args = function()
+      local benchmark_name = vim.fn.input('Benchmark: ')
+      return benchmark_args_fn(benchmark_name)
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+  },
+  {
+    name = "run-benchmark-bc",
+    type = "rust",
+    request = "launch",
+    program = function()
+      return vim.fn.getcwd() .. "/target/debug/som-interpreter-bc"
+    end,
+    args = function()
+      local benchmark_name = vim.fn.input('Benchmark: ')
+      return benchmark_args_fn(benchmark_name)
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+  },
+  {
+    name = "run-last-ast-benchmark",
+    type = "rust",
+    request = "launch",
+    program = function()
+      return vim.fn.getcwd() .. "/target/debug/som-interpreter-ast"
+    end,
+    args = function()
+      return benchmark_args_fn(vim.g.last_benchmark_ran)
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+  },
+  {
+    name = "run-last-bc-benchmark",
+    type = "rust",
+    request = "launch",
+    program = function()
+      return vim.fn.getcwd() .. "/target/debug/som-interpreter-bc"
+    end,
+    args = function()
+      return benchmark_args_fn(vim.g.last_benchmark_ran)
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+  },
+}
+
+function benchmark_args_fn(benchmark_name)
+  local classpath_items = {
+    "Smalltalk",
+    "Examples/Benchmarks",
+    "Examples/Benchmarks/LanguageFeatures",
+    "Examples/Benchmarks/Json",
+    "Examples/Benchmarks/Richards",
+    "Examples/Benchmarks/DeltaBlue"
+  }
+
+  local classpath = {}
+  for _, item in ipairs(classpath_items) do
+    table.insert(classpath, { "-c", "core-lib/" .. item })
+  end
+
+  table.insert(classpath, { "--", "core-lib/Examples/Benchmarks/BenchmarkHarness.som", benchmark_name, "1", "7" })
+
+  vim.g.last_benchmark_ran = benchmark_name
+
+  return vim.tbl_flatten(classpath)
+end

8f5a3b6dd9e53ebed3f8ff4d6a015863329cd9f7
Octave Larose <octave.larose@hotmail.fr>
2025-11-01 17:49:28 +0100

    dotfiles restoring script, finally

---
+++ copy_dotfiles_to_system.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+MSG_COLOR='\033[0;32m'
+WARN_COLOR='\033[0;33m'
+NO_COLOR='\033[0m'
+
+confirm_and_copy() {
+    local src=$1
+    local dest=$2
+    if [ -e "$dest" ]; then
+        read -p "Overwrite $dest? (y/n) " -n 1 -r
+        echo
+        if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+            return
+        fi
+    fi
+    cp -rv "$src" "$dest"
+}
+
+echo -e "${MSG_COLOR}### Restoring rc files...${NO_COLOR}"
+for file in .*rc; do
+    confirm_and_copy "$file" "$HOME/$file"
+done
+
+echo -e "${MSG_COLOR}\n### Restoring gitconfig...${NO_COLOR}"
+confirm_and_copy .gitconfig ~/.gitconfig
+
+echo -e "${MSG_COLOR}\n### No crontab backup at the moment (TODO!)...${NO_COLOR}"
+
+echo -e "${MSG_COLOR}\n### Restoring personal scripts...${NO_COLOR}"
+mkdir -p ~/bin
+for bin_name in bin/*; do 
+    confirm_and_copy $bin_name ~/bin/$(basename "$bin_name") 
+done
+
+echo -e "${MSG_COLOR}\n### Restoring config settings...${NO_COLOR}"
+for cfg in config/*; do
+    mkdir -p ~/.config/$(basename "$cfg") && confirm_and_copy "$cfg" ~/.config/$(basename "$cfg")
+done
+
+echo -e "${MSG_COLOR}\n### Restore complete!${NO_COLOR}"
